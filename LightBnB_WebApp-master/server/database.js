de0c091e46cd340c381a2c605596838b2814540d
Kaiya Hedley <keshedley@gmail.com>
2022-08-25 17:48:05 -0600

    implementing getallreservations function to work with database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -19,8 +19,11 @@ const getUserWithEmail = function(email) {
   return pool
   .query(`SELECT * FROM users WHERE email = $1`, [email])
   .then((result) => {
-    console.log(result.rows);
-    result.rows[0];
+    if (!result.rows.length) {
+      return null;
+    }
+    console.log(result.rows[0]);
+    return result.rows[0];
   })
   .catch((err) => {
     console.log(err.message);
@@ -35,9 +38,12 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 const getUserWithId = function(id) {
   return pool
-  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .query(`SELECT * FROM users WHERE users.id = $1`, [id])
   .then((result) => {
-    console.log(result.rows);
+    if (!result.rows.length) {
+      return null;
+    }
+    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
@@ -74,8 +80,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool
+  .query(`SELECT reservations.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = 1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date;`)
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
